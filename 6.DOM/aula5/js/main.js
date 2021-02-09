0eff39868696cf3ed20c3357b40dd3369c988b68
Fernando César <fernandocesar.alves@hotmail.com>
2021-02-09 00:08:49 -0300

    innerHTML

---
+++ 6.DOM/aula5/js/main.js
@@ -0,0 +1,4 @@
+(function() {
+    var nome = prompt('Qual o seu nome?');
+    document.querySelector('#msg').textContent = 'Bem-vindo, ' + nome;
+})();
---
+++ 6.DOM/aula6/js/main.js
@@ -0,0 +1,5 @@
+(function() {
+    var nome = prompt('Qual é o seu nome?');
+    document.querySelector('#msg').innerHTML = 'Bem-vindo, <b>' + nome + '</b>';
+
+})();
