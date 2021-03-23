38214fa57dd04a4a755dcad2660c47593ffcfe1e
Fernando César <fernandocesar.alves@hotmail.com>
2021-03-23 12:40:12 -0300

    metodos de array

---
+++ 13.Arrays/aula9/js/main.js
@@ -0,0 +1,36 @@
+(function(){
+    'use strict'
+    var trs = document.querySelectorAll('.js-tabela tbody .aluno');                
+    
+    var x = 0;
+    while(trs[x]){
+        var linhaAluno = trs[x];  
+        var tds = linhaAluno.querySelectorAll('td');
+        var notas = recuperarNumerosDeColunas(tds);  
+
+        /*
+        var temZero = notas.some(function(nota) {
+            return nota === 0; 
+        });
+        */
+        var temZero = notas.some(nota => nota === 0);
+        
+        var mensagem = temZero ? 'reprovado' : calcularMedia.apply(this, notas);
+            
+        
+        linhaAluno.querySelector('.aluno-m').textContent = mensagem;
+        x++;
+    }
+    
+    function recuperarNumerosDeColunas(tds){
+        var x= 0;
+        var arr = [];
+        while( tds[x] ){
+            if( !isNaN( tds[x].textContent ) && tds[x].textContent !== '' ){
+                arr.push( parseFloat(tds[x].textContent ) );
+            }
+            x++;
+        }
+        return arr;
+    }
+}) ();
